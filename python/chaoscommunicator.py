9e121a4c16c1a2dd62e9a3391a01219af02b2d4c
polysyllabic <karl@polysyllabic.com>
2021-07-25 10:42:36 -0700

    add GPL license headers

--- python/chaoscommunicator.py
+++ python/chaoscommunicator.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# This file is part of Twitch Controls Chaos (TCC).
+# Copyright 2021 blegas78
+#
+# TCC is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# TCC is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with TCC.  If not, see <https://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------
 # follows patterns from here: https://refactoring.guru/design-patterns/observer/python/example
 
 from __future__ import annotations
